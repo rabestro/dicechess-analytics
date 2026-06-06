98b29e353d111ba7be22052c68cbac80b94baec9
Jegors Čemisovs <jegors.cemisovs@gmail.com>
2026-06-06 19:33:19 +0300

    Refactor env.py for improved readability and organization

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,13 @@
+# add your model's MetaData object here
+# for 'autogenerate' support
+import os
+import sys
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from sqlalchemy import engine_from_config, pool
 
 from alembic import context
+from src.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -14,12 +18,10 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-import sys
-import os
+
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
-from src.models import Base
+
+
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
@@ -66,9 +68,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
